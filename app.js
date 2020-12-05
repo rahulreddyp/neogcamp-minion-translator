8b9d2e820bb5fa9c882b3c1ad6139e3a2d870123
rahulreddyp <rahulreddyp77@gmail.com>
2020-12-05 21:09:57 +0530

    style: added images and css

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("textarea[name='txt-input']")
+var txtInput = document.querySelector("textarea[name='txt-input']");
 
 
 var outputBox = document.querySelector('#output');
-console.log(outputBox);
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTramslationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("Error Occured:", error);
+    alert("Error occured! please try again after some time");
+}
+
 function clickHandler() {
-    outputBox.innerText = txtInput.value;
+    // outputBox.innerText = txtInput.value;
+    const inputText = txtInput.value; //taking input
+    // Calling server for processing
+    fetch(getTramslationURL(inputText))
+        .then(response => console.log(response.json()))
+            .then(json => {
+                var translatedText = json.contents.translated;
+                outputBox.innerText = translatedText // output
+            })
+        .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click", clickHandler);
